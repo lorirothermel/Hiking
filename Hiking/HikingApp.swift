49b59fee71f05816a21ff3d016b4769c9a80fec5
LBRothermel <128320877+LBRothermel@users.noreply.github.com>
2023-06-08 20:56:23 -0400

    Initial Commit

---
+++ Hiking/HikingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikingApp.swift
+//  Hiking
+//
+//  Created by Lori Rothermel on 6/8/23.
+//
+
+import SwiftUI
+
+@main
+struct HikingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
